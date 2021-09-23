8624641c1b521f2b0435f16af91f82c7f98101ce
millbergm <83matmil@skola.boras.se>
2021-09-23 10:22:32 +0200

    added mapScreen

--- screens/homeScreen.tsx
+++ screens/homeScreen.tsx
@@ -11,6 +11,7 @@ function HomeScreen({ onSetPage }: Props) {
             <Button title="Camera" onPress={() => onSetPage('camera')}/>
             <Button title="Info" onPress={() => onSetPage('info')}/>
             <Button title="Background" onPress={() => onSetPage('background')}/>
+            <Button title="Map" onPress={() => onSetPage('map')}/>
         </View>
     )
 }
