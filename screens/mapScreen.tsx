382d0c000799ca3eb05efa5bfdd0742a2d161712
millbergm <83matmil@skola.boras.se>
2021-09-23 12:06:38 +0200

    map centers on Borås

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -1,14 +1,35 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { useState } from 'react';
 
-function MapScreen (){
+interface IGeolocation {
+  latitude: number;
+  longitude: number;
+}
+
+// function MapScreen (){
+  
+  const MapScreen = () => {
+    const [location, setLocation] = useState<IGeolocation>({
+      latitude: 57.71717,
+      longitude: 12.94225,
+    });
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView 
+      style={styles.map} 
+      initialRegion={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.05,
+      }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
