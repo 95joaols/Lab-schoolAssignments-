59306e4e34fcb527dba4ffe61d46dc506b9a32c4
millbergm <83matmil@skola.boras.se>
2021-09-27 15:44:29 +0200

    removed commented code

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -24,22 +24,6 @@ function MapScreen({ onGoBack }: Props) {
     longitudeDelta: 0.01,
   });
 
-  // React.useEffect (() => {
-  //   Geolocation.getCurrentPosition(
-  //     position => {
-  //       const {latitude, longitude} = position.coords;
-  //       setLocation({
-  //         latitude,
-  //         longitude,
-  //       });
-  //     },
-  //     error => {
-  //       console.log(error.code, error.message);
-  //     },
-  //     {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-  //   );
-  // }, []);
-
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
