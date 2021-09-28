cd6a8af0b223a3bb00277cee7e600dc4884e0f9d
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-09-28 09:20:38 +0200

    - Added ScreenOrentationProvider to App.tsx again. Missed it in main merge.

--- App.tsx
+++ App.tsx
@@ -1,30 +1,33 @@
-import BackgroundSelectorScreen from "./screens/BackgroundSelectorScreen";
 import React from 'react';
-import HomeScreen from './screens/homeScreen';
-import MapScreen from './screens/mapScreen';
-import InfoScreen from './screens/InfoScreen';
-import CameraScreen from './screens/cameraScreen';
-import BackgroundImage from './components/BackgroundImage';
-import BackgroundImageProvider from './contexts/BackgroundImageContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { navigationTheme, navigatorTheme } from './constants/NavigationStyles';
+import BackgroundImageProvider from './contexts/BackgroundImageContext';
+import BackgroundImage from './components/BackgroundImage';
+import HomeScreen from './screens/homeScreen';
+import CameraScreen from './screens/cameraScreen';
+import InfoScreen from './screens/InfoScreen';
+import BackgroundSelectorScreen from "./screens/BackgroundSelectorScreen";
+import MapScreen from './screens/mapScreen';
+import ScreenOrientationProvider from "./contexts/ScreenOrientationContext";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <BackgroundImageProvider>
-      <NavigationContainer theme={navigationTheme}>
-        <BackgroundImage />
-        <Stack.Navigator screenOptions={navigatorTheme} >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Camera" component={CameraScreen} />
-          <Stack.Screen name="Info" component={InfoScreen} />
-          <Stack.Screen name="Background" component={BackgroundSelectorScreen} />
-          <Stack.Screen name="Map" component={MapScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </BackgroundImageProvider>
+    <ScreenOrientationProvider>
+      <BackgroundImageProvider>
+        <NavigationContainer theme={navigationTheme}>
+          <BackgroundImage />
+          <Stack.Navigator screenOptions={navigatorTheme} >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Camera" component={CameraScreen} />
+            <Stack.Screen name="Info" component={InfoScreen} />
+            <Stack.Screen name="Background" component={BackgroundSelectorScreen} />
+            <Stack.Screen name="Map" component={MapScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </BackgroundImageProvider>
+    </ScreenOrientationProvider>
   );
 }
