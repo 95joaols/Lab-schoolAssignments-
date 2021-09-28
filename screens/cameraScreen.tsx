95ad929cf583785535dd9cf1711c58c191dbd061
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-09-28 15:15:13 +0200

    - Added a slider for camera zoom. - Major overhaul of camera CSS.

--- screens/cameraScreen.tsx
+++ screens/cameraScreen.tsx
@@ -3,44 +3,15 @@ import { CameraType } from 'expo-camera/build/Camera.types';
 import React, { FC, useEffect, useState } from 'react';
 import { TouchableOpacity, Alert, Button, Text, View, StyleSheet } from 'react-native';
 import { requestPermissionsAsync, createAssetAsync } from 'expo-media-library';
+import Slider from '@react-native-community/slider';
 
-const styles = StyleSheet.create({  
-  errorContainer: {  
-    flex: 1,
-    backgroundColor: '#ffffff',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  camera: {
-    flex: 1,
-    alignSelf: 'stretch',
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  text: {
-    color: '#ffffff'
-  },
-  roundButton: {
-    width: 100,
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 100,
-    backgroundColor: 'red',
-  },
-  div: {
-    display: 'flex',
-    flexDirection: 'row'
-  }
-})
 
 const CameraScreen: FC = () => {
   const [cameraPermission, setCameraPermission] = useState<boolean | undefined> (undefined);
   const [libraryPermission, setLibraryPermission] = useState<boolean | undefined>(undefined);
   const [cameraType, setCameraType] = useState<CameraType> (Camera.Constants.Type.front);
   const [cameraReady, setCameraReady] = useState<boolean> (false);
+  const [cameraZoom, setCameraZoom] = useState<number>(0);
   let camera: Camera | null;
 
   const takePicture = async () => {
@@ -80,14 +51,85 @@ const CameraScreen: FC = () => {
   }
 
   return (
-    <Camera ratio={'16:9'} type={cameraType} ref={(reference) => {camera = reference}} style={styles.camera} useCamera2Api onCameraReady={() => {setCameraReady(true)}}>
-      {cameraType === Camera.Constants.Type.front ? <Text style={styles.text}>Selfie mode</Text> : <Text style={styles.text}>Boring mode</Text>}
-      <View style={styles.div}>
-        <Button title="Flip" onPress={() => {setCameraType (cameraType === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)}} />
-        <TouchableOpacity style={styles.roundButton} onPress={takePicture} />
+    <Camera ratio={'16:9'} zoom={cameraZoom} type={cameraType} ref={(reference) => {camera = reference}} style={styles.camera} useCamera2Api onCameraReady={() => {setCameraReady(true)}}>
+      {console.log('zoom: ', cameraZoom)}
+      <View style={styles.sliderDiv}>
+        <Slider
+          onSlidingComplete={(f) => {setCameraZoom(f)}}
+          style={styles.cameraSlider}
+          minimumValue={0}
+          maximumValue={1}
+          value={0}
+          minimumTrackTintColor="#ffffff"
+          thumbTintColor="#8E9080"
+          maximumTrackTintColor="#000000"
+        />
+      </View>
+      <View style={styles.buttonsDiv}>
+        <TouchableOpacity style={styles.cameraFlipButton} onPress={() => {setCameraType (cameraType === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)}}>
+          <Text style={styles.text}>Flip</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.cameraRoundButton} onPress={takePicture} />
       </View>
     </Camera>
   );
 };
+
+const styles = StyleSheet.create({
+  cameraSlider: {
+    width: 200,
+    height: 50,
+    marginRight: -75,
+    transform: [ { rotate: "-90deg" } ]
+  },
+  camera: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end'
+  },
+  sliderDiv: {
+    height: 200,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  buttonsDiv: {
+    display: 'flex',
+    minHeight: 100,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  errorContainer: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: '#ffffff'
+  },
+  cameraFlipButton: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+    marginRight: 190,
+    backgroundColor: '#8E9080',
+  },
+  cameraRoundButton: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: '#8E9080',
+  }
+})
 
 export default CameraScreen;
