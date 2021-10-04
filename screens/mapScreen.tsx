ee3595253baed00b58b79c7ed34e811111399050
millbergm <83matmil@skola.boras.se>
2021-10-04 15:19:11 +0200

    map now works with locationContext, added modal with info when location permission is denied

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useContext } from "react";
 import MapView, { Callout, Marker } from "react-native-maps";
-import { Text, View, TouchableHighlight } from "react-native";
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  Modal
+} from "react-native";
 import { styles } from "../constants/Styles";
 import { ScreenOrientationContext } from "../contexts/ScreenOrientationContext";
 import { Orientation } from "expo-screen-orientation";
@@ -11,6 +16,7 @@ function MapScreen() {
 
   const { screenOrientation } = useContext(ScreenOrientationContext);
 
+  const [modalVisible, setModalVisible] = useState(false);
   const [errorMsg, setErrorMsg] = useState(Location.errorMsg);
   const [region, setRegion] = useState({
     latitude: 57.72107,
@@ -19,9 +25,66 @@ function MapScreen() {
     longitudeDelta: 0.01,
   });
 
+  if (Location.errorMsg) {
+    setErrorMsg(Location.errorMsg);
+  }
+
   if (Location.grantedForeground?.status !== "granted") {
-    setErrorMsg("Disabled, No Permission");
-    return;
+    return (
+      <View style={styles.root}>
+        <MapView
+          style={
+            screenOrientation === Orientation.LANDSCAPE_RIGHT ||
+            screenOrientation === Orientation.LANDSCAPE_LEFT
+              ? styles.mapLandscape
+              : styles.map
+          }
+          region={region}
+        >
+          <Marker
+            coordinate={{
+              latitude: region.latitude,
+              longitude: region.longitude,
+            }}
+          />
+        </MapView>
+        <Callout style={styles.buttonsContainer}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.root}>
+              <View style={styles.modalView}>
+                <Text style={styles.buttonText}>
+                  Map needs permission to access Device Location. {"\n\n"}
+                  Go to Settings {">"} Apps & notifications {">"} Expo Go {">"}{" "}
+                  Permissions. Allow "Location".
+                </Text>
+                <TouchableHighlight
+                  onPress={() => setModalVisible(!modalVisible)}
+                  style={styles.button}
+                  underlayColor={"#B6B8A8"}
+                >
+                  <Text style={styles.buttonText}>Close</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </Modal>
+          <TouchableHighlight
+            onPress={() => setModalVisible(!modalVisible)}
+            style={styles.button}
+            underlayColor={"#B6B8A8"}
+          >
+            <Text style={styles.buttonText}>Get Location</Text>
+          </TouchableHighlight>
+        </Callout>
+      </View>
+    );
   }
 
   let text = "Waiting..";
@@ -32,17 +95,15 @@ function MapScreen() {
   }
 
   const getLocation = () => {
-    if (Location.grantedForeground?.status === "granted") {
-      if (Location.location !== undefined) {
-        setRegion({
-          latitude: Location.location?.coords.latitude,
-          longitude: Location.location?.coords.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        });
-      } else if (Location.location === undefined) {
-        text = "Location not available";
-      }
+    if (Location.location !== undefined) {
+      setRegion({
+        latitude: Location.location?.coords.latitude,
+        longitude: Location.location?.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+    } else if (Location.location === undefined) {
+      text = "Location not available";
     }
   };
 
@@ -75,6 +136,6 @@ function MapScreen() {
       </Callout>
     </View>
   );
-}
+};
 
 export default MapScreen;
